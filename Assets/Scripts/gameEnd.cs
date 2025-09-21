0e9505f87346f4b216a1ed6da32034ae0ce7eb1f
Ale1as <161304725+Ale1as@users.noreply.github.com>
2025-09-21 13:37:06 +0530

    alpha updated

--- Assets/Scripts/gameEnd.cs
+++ Assets/Scripts/gameEnd.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class gameEnd : MonoBehaviour
 {
     public GameObject player;
+    [SerializeField] GameObject outroimage;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             player.GetComponent<Movement>().enabled = false;
             FindAnyObjectByType<Camera>().GetComponent<FPScamera>().enabled = false;
+            outroimage.SetActive(true);
         }
     }
 }
